2fc0b805a7c85de6c262530699d6a0b9a1e96518
yubar46 <yubar4.6@gmail.com;git config --global user.email yubar4.6@gmail.com>
2022-05-23 16:03:00 +0430

    edit method AddToPastOrders in interface PastOrdersRepository; add method selectPastOrders into interface

--- crc/repository/PastOrdersRepository.java
+++ crc/repository/PastOrdersRepository.java
@@ -1,10 +1,12 @@
 package repository;
 
 import domain.PastOrders;
+import domain.User;
 
 import java.sql.SQLException;
 
 public interface PastOrdersRepository {
 
-    public void AddToPastOrders(PastOrders pastOrders) throws SQLException;
+    public void AddToPastOrders(PastOrders pastOrders, User user) throws SQLException;
+    public void selectPastOrders(int userId) throws SQLException;
 }
