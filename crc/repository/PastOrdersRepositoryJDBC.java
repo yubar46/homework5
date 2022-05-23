544edd54f8edf7edc0f00eeba5b877f15b029ed3
yubar46 <yubar4.6@gmail.com;git config --global user.email yubar4.6@gmail.com>
2022-05-23 16:05:39 +0430

    implement method selectPastOrders

--- crc/repository/PastOrdersRepositoryJDBC.java
+++ crc/repository/PastOrdersRepositoryJDBC.java
@@ -1,6 +1,7 @@
 package repository;
 
 import domain.PastOrders;
+import domain.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -14,7 +15,7 @@ public class PastOrdersRepositoryJDBC implements  PastOrdersRepository {
         this.connection = connection;
     }
 
-    public void AddToPastOrders(PastOrders pastOrders) throws SQLException {
+    public void AddToPastOrders(PastOrders pastOrders, User user) throws SQLException {
         String add = "INSERT INTO PastOrders (Product, price, Adress, User_id) VALUES (?,?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(add);
         preparedStatement.setString(1,pastOrders.getProduct().getName());
@@ -32,5 +33,21 @@ public class PastOrdersRepositoryJDBC implements  PastOrdersRepository {
 
 
 
+    }
+
+    public void selectPastOrders(int userId) throws SQLException {
+        String selectPastOrders ="select *from PastOrders where User_id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(selectPastOrders);
+        preparedStatement.setInt(1,userId);
+        ResultSet resultSet = preparedStatement.executeQuery();
+            boolean empty=true;
+        while (resultSet.next()){
+            System.out.print("product name : "+ resultSet.getString(2)+"\t");
+            System.out.print("price: "+ resultSet.getInt(3)+"\t");
+            System.out.print(resultSet.getString(4));
+            System.out.println("order price ; "+resultSet.getInt(5));
+            empty=false;
+        }
+        if (empty) System.out.println("you have any order in past");
     }
 }
